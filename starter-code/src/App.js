a9f612d0418309e8814de5aa32d712aecd6e1385
unknown <culaficstefan@gmail.com>
2019-10-09 15:11:58 +0200

    example

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,18 +1,60 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bulma/css/bulma.css';
+import foods from './foods.json';
+import FoodBox from './components/FoodBox';
 
 class App extends Component {
+  state = {
+    foods: foods,
+    addNew: false
+  }
+
+  formSubmitHandler = ( event ) => {
+    event.preventDefault();
+    this.setState({
+      foods: [{
+        name: event.target.name.value,
+        calories: event.target.calories.value,
+        image: event.target.image.value,
+        quantity: 0
+      }].concat(this.state.foods)
+    })
+  }
+  
+  toggleFormHandler = () => {
+    this.setState({
+      addNew: !this.state.addNew
+    })
+  }
+
   render() {
+    const addNewFood = (
+      <button onClick={this.toggleFormHandler}>addNewFood</button>
+    )
+
+    const formToRender = (
+      <form onSubmit={this.formSubmitHandler}>
+        <label>Name</label>
+        <input type="text" name="name"></input>
+        <label>Calories</label>
+        <input type="text" name="calories"></input>
+        <label>Image</label>
+        <input type="text" name="image"></input>
+        <button type="submit">Submit</button>
+      </form>
+    )
+    
+
+
+    const foodsToRender = this.state.foods.map((item,index) => {
+      return <FoodBox foodData={item}  key={index}/>
+    })
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {this.state.addNew && formToRender}
+        {!this.state.addNew && addNewFood}
+        {foodsToRender}
       </div>
     );
   }
